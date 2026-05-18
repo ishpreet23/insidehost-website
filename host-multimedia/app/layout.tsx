394fb3f46dfb24f8343124026e62bd1c19244a4d
ishpreet23 <ishpreetsaluja2004@gmail.com>
2026-05-18 21:55:25 +0530

    Added Google Analytics integration

--- host-multimedia/app/layout.tsx
+++ host-multimedia/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 
 import { Geist, Geist_Mono } from "next/font/google";
 
+import Script from "next/script";
+
 import "./globals.css";
 
 import "@fontsource/josefin-sans/300.css";
@@ -48,6 +50,23 @@ export default function RootLayout({
           overflow-x-hidden
         "
       >
+
+        {/* GOOGLE ANALYTICS */}
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-H4VJ5XFF2S"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-H4VJ5XFF2S');
+          `}
+        </Script>
 
         {/* PREMIUM TOASTER */}
 
